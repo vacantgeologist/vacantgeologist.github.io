fec1cc984a0b1cca08576b06ce966467f0454e73
vacantgeologist <jwcubnxxit@protonmail.com>
2017-05-05 15:45:33 +0900

    Animate greeting

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,32 @@
 require('normalize.css');
 require('../style/style.less');
-var hello = 'Hi';
+const $ = require('jquery');
 
-console.log(hello);
+// variables
+let showCursor = false;
+let revealCounter = 0;
+let greetingMessage = 'hi({ to: you });'.split("");
+
+$(document).ready(function() {
+  var revealer = setInterval(function(){
+    if (revealCounter < greetingMessage.length) {
+      revealCounter ++;
+      let textToShow = greetingMessage.slice(0, revealCounter).join('');
+      $("#greetingText").text(textToShow);
+    } else {
+      clearInterval(revealer);
+    }
+  }, 80);
+  setInterval(blinkCursor, 500);
+});
+
+function blinkCursor() {
+  showCursor = !showCursor;
+  if (showCursor) {
+    $("#cursor").show();
+    $("#space").hide();
+  } else {
+    $("#cursor").hide();
+    $("#space").show();
+  }
+}
